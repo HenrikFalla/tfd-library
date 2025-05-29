93a33ae7acc1804705841ef4ddfc7d92572ca766
HenrikFalla <henrik.falla@gmail.com>
2025-05-29 12:49:13 +0200

    Fixed case sensitivity in data fetching

--- src/app/api/metadata/get-descendant/route.ts
+++ src/app/api/metadata/get-descendant/route.ts
@@ -5,14 +5,19 @@ const prisma = PrismaGlobalClient;
 export async function GET(request: NextRequest) {
 	const searchParams = request.nextUrl.searchParams;
 	const name = searchParams.get('name') as string;
-	const req = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
-	const data = await GetDescendant(req);
+	console.log('Name: ', name);
+	// const req = name.charAt(0).toUpperCase() + name.slice(1).toLowerCase();
+	const data = await GetDescendant(name);
+	console.log('Response Data: ', data);
 	return NextResponse.json(data);
 }
 async function GetDescendant(name: string) {
 	const data = await prisma.descendant.findFirst({
 		where: {
-			descendant_name: name,
+			descendant_name: {
+				equals: name,
+				mode: 'insensitive',
+			},
 		},
 		include: {
 			descendant_skill: true,
